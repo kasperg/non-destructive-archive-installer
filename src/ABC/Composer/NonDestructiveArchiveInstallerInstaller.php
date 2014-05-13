1bd2bc1868db9175fbceb9b65a09a13732ae2eff
Kasper Garnaes <kasper.garnaes@gmail.com>
2014-05-13 11:12:04 +0200

    Move downloaded package instead of redownloading

--- src/ABC/Composer/NonDestructiveArchiveInstallerInstaller.php
+++ src/ABC/Composer/NonDestructiveArchiveInstallerInstaller.php
@@ -2,14 +2,14 @@
 
 namespace ABC\Composer;
 
-use Composer\Util\RemoteFilesystem;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Installer;
 use Composer\Repository\InstalledRepositoryInterface;
-use Composer\Factory;
 use Composer\Package\PackageInterface;
 use Composer\Installer\LibraryInstaller;
+use Composer\Util\Filesystem;
+use Composer\Util\ProcessExecutor;
 
 /**
  * This class is in charge of handling the installation of an external package
@@ -32,7 +32,7 @@ class NonDestructiveArchiveInstallerInstaller extends LibraryInstaller {
     public function __construct(IOInterface $io, Composer $composer, $type = 'library')
     {
         parent::__construct($io, $composer, $type);
-        $this->rfs = new RemoteFilesystem($io);
+        $this->fs = new Filesystem(new ProcessExecutor($io));
     }
 
     /**
@@ -42,7 +42,7 @@ class NonDestructiveArchiveInstallerInstaller extends LibraryInstaller {
     {
         parent::update($repo, $initial, $package);
 
-        $this->downloadAndExtractFile($package);
+        $this->moveDownloadedPackage($package);
     }
 
     /**
@@ -52,17 +52,17 @@ class NonDestructiveArchiveInstallerInstaller extends LibraryInstaller {
     {
         parent::install($repo, $package);
 
-        $this->downloadAndExtractFile($package);
+        $this->moveDownloadedPackage($package);
     }
 
     /**
-     * Downloads and extracts the package, only if the URL to download has not been downloaded before.
+     * Move the package, only if the URL to download has not been downloaded before.
      *
      * @param PackageInterface $package
      * @throws \RuntimeException
      * @throws \UnexpectedValueException
      */
-    private function downloadAndExtractFile(PackageInterface $package) {
+    private function moveDownloadedPackage(PackageInterface $package) {
 
         // get extra data
         $c_extra = $this->composer->getPackage()->getExtra();
@@ -96,42 +96,18 @@ class NonDestructiveArchiveInstallerInstaller extends LibraryInstaller {
                 }
             }
 
-            // First, try to detect if the archive has been downloaded
-            // If yes, do nothing.
-            // If no, let's download the package.
+            // If the archive has been downloaded then do nothing
             if (self::getLastDownloadedFileUrl($package) == $url) {
                 return;
             }
 
-            // Download (using code from FileDownloader)
-            $fileName = pathinfo(parse_url($url, PHP_URL_PATH), PATHINFO_BASENAME);
-            $extension = strtolower(pathinfo(parse_url($url, PHP_URL_PATH), PATHINFO_EXTENSION));
-
-            if (!extension_loaded('openssl') && 0 === strpos($url, 'https:')) {
-                throw new \RuntimeException('You must enable the openssl extension to download files via https');
+            // Move all downloaded files to the target directory.
+            $installDir = $this->getInstallPath($package) . '/';
+            foreach (scandir($installDir) as $file) {
+                if (!in_array($file, array('.', '..', 'download-status.txt'))) {
+                    $this->filesystem->rename($installDir . $file, $targetDir);
+                }
             }
-
-
-            $this->io->write("    - Downloading <info>" . $fileName . "</info> from <info>".$url."</info>");
-
-            $this->rfs->copy(parse_url($url, PHP_URL_HOST), $url, $fileName);
-
-            if (!file_exists($fileName)) {
-                throw new \UnexpectedValueException($url.' could not be saved to '.$fileName.', make sure the'
-                .' directory is writable and you have internet connectivity');
-            }
-
-            // Extract using ZIP downloader
-            if ($extension == 'zip') {
-                $this->io->write("    - Extracting <info>" . $fileName . "</info> to <info>" . $targetDir . "</info>");
-                $this->extractZip($fileName, $targetDir, $omitFirstDirectory);
-            } elseif ($extension == 'tar' || $extension == 'gz' || $extension == 'bz2') {
-                $this->io->write("    - Extracting <info>" . $fileName . "</info> to <info>" . $targetDir . "</info>");
-                $this->extractTgz($fileName, $targetDir, $omitFirstDirectory);
-            }
-
-            // Delete archive once download is performed
-            unlink($fileName);
 
             // Save last download URL
             self::setLastDownloadedFileUrl($package, $url);
@@ -175,7 +151,7 @@ class NonDestructiveArchiveInstallerInstaller extends LibraryInstaller {
      * @param PackageInterface $package
      * @param unknown $url
      */
-    public static function setLastDownloadedFileUrl(PackageInterface $package, $url) {
+    protected static function setLastDownloadedFileUrl(PackageInterface $package, $url) {
         $packageDir = self::getPackageDir($package);
         file_put_contents($packageDir."download-status.txt", $url);
     }
@@ -186,103 +162,9 @@ class NonDestructiveArchiveInstallerInstaller extends LibraryInstaller {
      * @param PackageInterface $package
      * @return string
      */
-    public static function getPackageDir(PackageInterface $package) {
+    protected static function getPackageDir(PackageInterface $package) {
         return __DIR__."/../../../../../".$package->getName()."/";
     }
 
-    /**
-     * Extract ZIP (copied from Composer's ZipDownloader)
-     *
-     * @param string $file
-     * @param string $path
-     * @throws \RuntimeException
-     * @throws \UnexpectedValueException
-     */
-    protected function extractZip($file, $path, $omitFirstDirectory)
-    {
-        if (!class_exists('ZipArchive')) {
-            throw new \RuntimeException('You need the zip extension enabled to use the ZipDownloader');
-        }
 
-        $zipArchive = new \ZipArchive();
-
-        if (true !== ($retval = $zipArchive->open($file))) {
-            throw new \UnexpectedValueException("Unable to open downloaded ZIP file.");
-        }
-
-        if ($omitFirstDirectory) {
-            $this->extractIgnoringFirstDirectory($zipArchive, $path);
-        } else {
-            if (true !== $zipArchive->extractTo($path)) {
-                throw new \RuntimeException("There was an error extracting the ZIP file. Corrupt file?");
-            }
-        }
-
-        $zipArchive->close();
-    }
-
-    /**
-     * Extract the ZIP file, but ignores the first directory of the ZIP file.
-     * This is useful if you want to extract a ZIP file that contains all the content stored
-     * in one directory and that you don't want this directory.
-     *
-     * @param unknown $zipArchive
-     * @param unknown $path
-     * @throws \Exception
-     */
-    protected function extractIgnoringFirstDirectory($zipArchive, $path) {
-        for( $i = 0; $i < $zipArchive->numFiles; $i++ ){
-            $stat = $zipArchive->statIndex( $i );
-
-            $filename = $stat['name'];
-
-            $pos = strpos($filename, '/');
-            if ($pos !== false) {
-                // The file name, without the the directory
-                $newfilename = substr($filename, $pos+1);
-            } else {
-                $newfilename = $filename;
-            }
-
-            if (!$newfilename) {
-                continue;
-            }
-
-            $fp = $zipArchive->getStream($filename);
-            if  (!$fp) {
-                throw new \Exception("Unable to read file $filename from archive.");
-            }
-
-            if (!file_exists(dirname($path.$newfilename))) {
-                mkdir(dirname($path.$newfilename), 0777, true);
-            }
-
-            // If the current file is actually a directory, let's pass.
-            if (strrpos($newfilename, '/') == strlen($newfilename)-1) {
-                continue;
-            }
-
-            $fpWrite = fopen($path.$newfilename, "wb");
-
-            while (!feof($fp)) {
-                fwrite($fpWrite, fread($fp, 65536));
-            }
-        }
-    }
-
-    /**
-     * Extract tar, tar.gz or tar.bz2 (copied from Composer's TarDownloader)
-     *
-     * @param string $file
-     * @param string $path
-     */
-    protected function extractTgz($file, $path, $omitFirstDirectory)
-    {
-        if ($omitFirstDirectory) {
-            throw new \Exception("Sorry! The omit-first-directory parameter is currently only supported for ZIP files.");
-        }
-        // Can throw an UnexpectedValueException
-        $archive = new \PharData($file);
-        $archive->extractTo($path, null, true);
-    }
 }
